1e9629bea352a752ce05830e58bf0b1fedc3d31a
Necollaz <156367647+Necollaz@users.noreply.github.com>
2024-08-09 11:23:44 +0300

    1.6

--- Scripts/Base/FlagPlacer.cs
+++ Scripts/Base/FlagPlacer.cs
@@ -8,6 +8,16 @@ public class FlagPlacer : MonoBehaviour
 
     private void Update()
     {
+        HandleClick();
+    }
+
+    private void SelectBase(Base baseComponent)
+    {
+        _base = baseComponent;
+    }
+
+    private void HandleClick()
+    {
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -16,27 +26,32 @@ public class FlagPlacer : MonoBehaviour
             {
                 if (hit.collider.TryGetComponent(out Base baseComponent))
                 {
-                    _base = baseComponent;
+                    SelectBase(baseComponent);
                 }
                 else if (_base != null && hit.collider.TryGetComponent(out Ground ground))
                 {
-                    Vector3 hitPoint = hit.point;
-
-                    if (ground.TryGetComponent(out Collider collider) && collider .bounds.Contains(hitPoint))
-                    {
-                        if (_base.FlagInstance == null)
-                        {
-                            _base.FlagInstance = Instantiate(_prefab, hitPoint, Quaternion.identity);
-                        }
-                        else
-                        {
-                            _base.FlagInstance.SetPosition(hitPoint);
-                        }
-
-                        _base = null;
-                    }
+                    Place(hit, ground);
                 }
             }
+        }
+    }
+
+    private void Place(RaycastHit hit, Ground ground)
+    {
+        Vector3 hitPoint = hit.point;
+
+        if (ground.TryGetComponent(out Collider collider) && collider.bounds.Contains(hitPoint))
+        {
+            if (_base.FlagInstance == null)
+            {
+                _base.FlagInstance = Instantiate(_prefab, hitPoint, Quaternion.identity);
+            }
+            else
+            {
+                _base.FlagInstance.SetPosition(hitPoint);
+            }
+
+            _base = null;
         }
     }
 }
